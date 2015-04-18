36ddcf219a77b65ad51bcb1b1eecac2e89736b3a
almyu <hire@indeed.li>
2015-04-18 09:40:16 +0600

    Add follow script

---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class FollowTarget : MonoBehaviour {
+
+    public NavMeshAgent agent;
+    public Transform target;
+
+    private void Update() {
+        agent.destination = target.position;
+    }
+}
