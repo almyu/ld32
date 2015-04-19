02dfea661cedf0cf6e0f98897721b54115073e05
almyu <hire@indeed.li>
2015-04-20 01:07:16 +0600

    Let more folks in

--- Assets/Scripts/Entrance.cs
+++ Assets/Scripts/Entrance.cs
@@ -2,7 +2,7 @@
 
 public class Entrance : MonoBehaviour {
 
-    public GameObject visitorPrefab;
+    public GameObject[] visitorPrefabs;
     public Vector2 interval = new Vector2(3f, 5f);
     public float rate = 1f;
 
@@ -13,12 +13,9 @@ public class Entrance : MonoBehaviour {
 
         nextVisitTime = Time.timeSinceLevelLoad + Random.Range(interval[0], interval[1]) / rate;
 
-        var obj = (GameObject) Instantiate(visitorPrefab, transform.position, transform.rotation);
+        var prefab = visitorPrefabs[Random.Range(0, visitorPrefabs.Length)];
+
+        var obj = (GameObject)Instantiate(prefab, transform.position, transform.rotation);
         obj.name += "" + Random.Range(0, 30);
-        
-        var target = obj.AddComponent<Target>();
-        target.enabled = false;
-        target.faction = Random.Range(0, Target.numFactions);
-        target.enabled = true;
     }
 }
