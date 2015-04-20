dd0fe2b6424ff27b0e5247e8e0f6e84c520477d7
almyu <hire@indeed.li>
2015-04-21 00:31:31 +0600

    Add grid logic

--- Assets/Scripts/Ikea.cs
+++ Assets/Scripts/Ikea.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class Ikea : MonoBehaviour {
 
     public Grid grid;
     public GameObject prefab;
     public Shader previewShader;
-    public Color previewTint;
+    public Color previewGoodTint, previewBadTint;
+    public Material cellGoodMaterial, cellBadMaterial;
 
     private GameObject preview;
+    private Material[] previewMaterials = new Material[0];
+    private int gridWidth, gridDepth;
+    public Vector3 offset;
 
     public void CleanupPreview() {
         if (preview) DestroyImmediate(preview);
@@ -20,6 +25,7 @@ public class Ikea : MonoBehaviour {
         if (!prefab) return;
 
         var xf = transform;
+        var mtls = new List<Material>();
 
         preview = (GameObject) Instantiate(prefab, xf.position, xf.rotation);
         preview.transform.SetParent(xf);
@@ -28,8 +34,11 @@ public class Ikea : MonoBehaviour {
             var mtl = ren.material;
             mtl.shader = previewShader;
             mtl.renderQueue = 3100;
-            mtl.SetColor("_TintColor", previewTint);
+
+            mtls.Add(mtl);
         }
+
+        previewMaterials = mtls.ToArray();
 
         foreach (var cmp in preview.GetComponentsInChildren<Component>()) {
             if (cmp is Transform) continue;
@@ -38,10 +47,20 @@ public class Ikea : MonoBehaviour {
 
             DestroyImmediate(cmp);
         }
+
+        var bounds = GatherVisualBounds(preview);
+        var size = bounds.size;
+
+        gridWidth = Mathf.Max(1, Mathf.RoundToInt(size.x));
+        gridDepth = Mathf.Max(1, Mathf.RoundToInt(size.z));
+
+        offset = new Vector3((gridWidth - 1) % 2, 0, (gridDepth - 1) % 2) * 0.5f;
+
+        preview.transform.localPosition = offset;
     }
 
     public void BuildHere() {
-        Instantiate(prefab, transform.position, transform.rotation);
+        Instantiate(prefab, transform.position + offset, transform.rotation);
     }
 
     private static Bounds GatherVisualBounds(GameObject obj) {
@@ -68,8 +87,21 @@ public class Ikea : MonoBehaviour {
         grid.GetHoveredCell(out x, out z);
         transform.position = grid.CellToWorldPoint(x, z);
 
-        if (Input.GetMouseButtonUp(0)) {
+        var good = true;
+
+        grid.ResetMaterials();
+        grid.Apply(x, z, gridWidth, gridDepth, (x_, z_, state, cellRen) => {
+            var goodCell = (state & 1) == 0;
+            good = good && goodCell;
+            cellRen.sharedMaterial = goodCell ? cellGoodMaterial : cellBadMaterial;
+        });
+
+        foreach (var mtl in previewMaterials)
+            mtl.SetColor("_TintColor", good ? previewGoodTint : previewBadTint);
+
+        if (good && Input.GetMouseButtonUp(0)) {
             BuildHere();
+            grid.Apply(x, z, gridWidth, gridDepth, (x_, z_, state, cellRen) => grid.SetStateBit(x_, z_, 1, true));
         }
     }
 
