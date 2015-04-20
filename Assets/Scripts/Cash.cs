eff3b8457d45b3216900dfd6b82a0a944dd25655
almyu <hire@indeed.li>
2015-04-21 03:23:20 +0600

    Add cash script

---
+++ Assets/Scripts/Cash.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using JamSuite.Events;
+
+public class Cash : MonoSingleton<Cash> {
+
+    public int amount = 100;
+    public IntEvent onChange;
+
+    public FloatingText notifier;
+    public Vector3 transactionPosition;
+
+    public bool Spend(int some) {
+        if (amount < some) return false;
+
+        amount -= some;
+        onChange.Invoke(amount);
+        return true;
+    }
+
+    public void Earn(int some) {
+        amount += some;
+        onChange.Invoke(amount);
+    }
+
+    public void Notify(int value) {
+        if (notifier) notifier.Spawn(transactionPosition, value);
+    }
+}
